b49463a8b14c46f36f2044d554db86cc4535a03c
Bing Xiao <l.h.xiao@gmail.com>
2020-12-22 10:59:16 -0500

    Add a c# gRPC starwars project

---
+++ cstarwars/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Grpc.Core;
+using Main;
+
+namespace cstarwars
+{
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      // Build the server
+      Console.WriteLine("Starting C# server on port 9002");
+      Server server = new Server {
+          Services = { starwars.BindService(new StarwarsService()) },
+          Ports = { new ServerPort("localhost", 9002, ServerCredentials.Insecure)}
+      };
+      server.Start();
+
+      // Call the Go server on port 9001
+      // Set up gRPC client
+      Channel channel = new Channel("localhost:9001", ChannelCredentials.Insecure);
+      var client = new starwars.starwarsClient(channel);
+
+      Console.WriteLine("Enter the command to call the Go server...");
+      Console.WriteLine("   c/name- search characters");
+      Console.WriteLine("   h/name - get hero by episode");
+      Console.WriteLine("   k/name - create review");
+      Console.WriteLine("   r/name - get reviews by episode");
+      Console.WriteLine("   x - exit");
+
+      for (; ; )
+      {
+        var command = Console.ReadLine();
+        var s = command.Split("/");
+
+        if (s[0].Equals("x")) break;
+
+        Episode episode;
+        switch (s[0])
+        {
+          case "c":
+            var resp = client.SearchCharacter(new SearchCharacterRequest {Name = s[1]});
+            Console.WriteLine("characters: {0}", resp);
+            break;
+          case "h":
+            Enum.TryParse(s[1], out episode);
+            var hero = client.GetHero(new GetHeroRequest {Episode = episode});
+            Console.WriteLine("hero: {0}", hero);
+            break;
+          case "k":
+            if (s.Length != 4) break;
+
+            Enum.TryParse(s[1], out episode);
+            int.TryParse(s[2], out int stars);
+            var review = new Review {Stars = stars, Commentary = s[3], Episode = episode};
+            var revResp = client.AddReview(review);
+            Console.WriteLine("review: {0}", revResp);
+            break;
+          case "r":
+            Enum.TryParse(s[1], out episode);
+            var reviewResp = client.GetReviews(new GetReviewsRequest {Episode = episode });
+            Console.WriteLine("reviews: {0}", reviewResp);
+            break;
+        }
+      }
+
+      // Block for server termination
+      Console.ReadKey();
+      channel.ShutdownAsync().Wait();
+      server.ShutdownAsync().Wait();
+    }
+  }
+}
---
+++ cstarwars/StarwarsService.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Main;
+
+namespace cstarwars
+{
+  public class StarwarsService : starwars.starwarsBase
+  {
+    private readonly Dictionary<int, Character> _characters;
+
+    private readonly Dictionary<Episode, List<Review>> _data = new Dictionary<Episode, List<Review>>();
+
+    public StarwarsService()
+    {
+      _characters = CreateCharacters().ToDictionary(t => t.Id);
+    }
+
+    public override Task<Character> GetHero(GetHeroRequest req, ServerCallContext context)
+    {
+      Console.WriteLine("Incoming get hero request: Episode = {0} " + req.Episode);
+
+      var response = req.Episode == Episode.Empire ? _characters[1000] : _characters[2001];
+
+      return Task.FromResult(response);
+    }
+
+    public override Task<SearchCharacterResponse> SearchCharacter(SearchCharacterRequest req, ServerCallContext context)
+    {
+      Console.WriteLine("Incoming search character request: name = {0} ", req.Name);
+
+      var resp = new SearchCharacterResponse();
+      foreach (var character in _characters.Values
+        .Where(t => (!string.IsNullOrEmpty(req.Name) && t.Name.Contains(req.Name, StringComparison.OrdinalIgnoreCase)) || t.Id == req.Id))
+      {
+        resp.Characters.Add(character);
+      }
+
+      return Task.FromResult(resp);
+    }
+
+    public override Task<Review> AddReview(Review review, ServerCallContext context)
+    {
+      Console.WriteLine("Incoming add review request: {0} : {1}, {2} ", review.Episode, review.Stars, review.Commentary);
+
+      if (!_data.TryGetValue(review.Episode, out List<Review> reviews))
+      {
+        reviews = new List<Review>();
+        _data[review.Episode] = reviews;
+      }
+
+      reviews.Add(review);
+
+      return Task.FromResult(review);
+    }
+
+    public override Task<GetReviewsResponse> GetReviews(GetReviewsRequest req, ServerCallContext context)
+    {
+      Console.WriteLine("Incoming get reviews request: Episode = {0} ", req.Episode);
+
+      var resp = new GetReviewsResponse();
+      if (_data.TryGetValue(req.Episode, out List<Review> reviews))
+      {
+        resp.Reviews.AddRange(reviews);
+      }
+
+      return Task.FromResult(resp);
+    }
+
+    private static IEnumerable<Character> CreateCharacters()
+    {
+      yield return new Character
+      {
+        Id = 1000,
+        Name = "Luke Skywalker",
+        Friends = { "Han Solo", "Leia Organa", "C-3PO", "R2-D2" },
+        AppearsIn = { Episode.NewHope, Episode.Empire, Episode.Jedi },
+        HomePlanet = "Tatooine"
+      };
+
+      yield return new Character
+      {
+        Id = 1001,
+        Name = "Darth Vader",
+        Friends = { "Wilhuff Tarkin" },
+        AppearsIn = { Episode.NewHope, Episode.Empire, Episode.Jedi },
+        HomePlanet = "Tatooine"
+      };
+
+      yield return new Character
+      {
+        Id = 1002,
+        Name = "Han Solo",
+        Friends = { "Luke Skywalker", "Leia Organa", "C-3PO" },
+        AppearsIn = { Episode.NewHope, Episode.Empire, Episode.Jedi },
+      };
+
+      yield return new Character
+      {
+        Id = 1003,
+        Name = "Leia Organa",
+        Friends = { "Han Solo", "Luke Skywalker", "C-3PO", "R2-D2" },
+        AppearsIn = { Episode.NewHope, Episode.Empire, Episode.Jedi },
+        HomePlanet = "Alderaan"
+      };
+
+      yield return new Character
+      {
+        Id = 1004,
+        Name = "Wilhuff Tarkin",
+        Friends = { "Darth Vader" },
+        AppearsIn = { Episode.NewHope }
+      };
+
+      yield return new Character
+      {
+        Id = 2000,
+        Name = "C-3PO",
+        Friends = { "Han Solo", "Luke Skywalker", "Leia Organa", "R2-D2" },
+        AppearsIn = { Episode.NewHope, Episode.Empire, Episode.Jedi },
+        PrimaryFunction = "Protocol"
+      };
+
+      yield return new Character
+      {
+        Id = 2001,
+        Name = "R2-D2",
+        Friends = { "Han Solo", "Luke Skywalker", "C-3PO" },
+        AppearsIn = { Episode.NewHope, Episode.Empire, Episode.Jedi },
+        PrimaryFunction = "Astromech"
+      };
+    }
+  }
+}
